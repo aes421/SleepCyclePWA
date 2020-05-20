eda43ab862ae129bbeceddcf83f1209b29e7a1ed
Ashley Stallings Rhodes <aesdev421@gmail.com>
2020-05-19 20:10:17 -0500

    Scaffold PWA and add basic inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'sleep-cycle-calculator';
+  hours = Array.from({ length: 12 }, (v, k) => String(k + 1).padStart(2, '0'));
+  minutes = Array.from({ length: (60 / 5) }, (v, k) => String(k * 5).padStart(2, '0'));
+  periods = ['AM', 'PM'];
 }
