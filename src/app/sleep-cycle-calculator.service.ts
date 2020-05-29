2c254f5927f2a1cd6e3648234079e2c005fc2bc5
Ashley Stallings Rhodes <aesdev421@gmail.com>
2020-05-28 20:28:51 -0500

    Refactor unit tests and calculate the total time asleep

--- src/app/sleep-cycle-calculator.service.ts
+++ src/app/sleep-cycle-calculator.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import * as moment from 'moment';
+import { Suggestion } from 'src/models/suggestion';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,12 @@ export class SleepCycleCalculatorService {
 
   constructor() { }
 
-  calculateBedtime(targetTime: moment.Moment): moment.Moment[] {
-    return Array.from({ length: 3 }, (v, k) => targetTime.clone().subtract(90 * (6 - k), 'minutes'));
+  calculateBedtime(targetTime: moment.Moment): Suggestion[] {
+    return Array.from({ length: 3 }, (v, k) => {
+      const bedtime = targetTime.clone().subtract(90 * (6 - k), 'minutes');
+      return {
+        moment: bedtime, elapsed: targetTime.diff(bedtime, 'hours', true)
+      }
+    });
   }
 }
